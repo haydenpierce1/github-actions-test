83901e5f41a2ee3973d08fd1357a8fb98ae0aa4d
Hayden Pierce <hayden.pierce@spoton.com>
2023-10-11 19:25:21 -0500

    add application

--- src/build.php
+++ src/build.php
@@ -1,5 +1,35 @@
 <?php
 
-echo "hello world";
+// Get real path for our folder
+$rootPath = realpath(__DIR__ . '/..');
 
-file_put_contents(__DIR__ . '/../build.txt', 'This is the build');
+// Initialize archive object
+$zip = new ZipArchive();
+$name = __DIR__ . '/../build/prod' . time() . '.zip';
+$zip->open($name, ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+// Create recursive directory iterator
+/** @var SplFileInfo[] $files */
+$files = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator($rootPath),
+    RecursiveIteratorIterator::LEAVES_ONLY
+);
+
+foreach ($files as $name => $file)
+{
+    // Skip directories (they would be added automatically)
+    if (!$file->isDir())
+    {
+        // Get real and relative path for current file
+        $filePath = $file->getRealPath();
+        $relativePath = substr($filePath, strlen($rootPath) + 1);
+
+        // Add current file to archive
+        $zip->addFile($filePath, $relativePath);
+    }
+}
+
+// Zip archive will be created only after closing object
+$zip->close();
+
+echo $name . "\n";
